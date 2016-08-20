8fcac158b5c1ce99290d273f4c51f40afa73e956
liu-seldon <1003037352@qq.com>
2016-08-20 15:28:30 +0800

    some new test

---
+++ Hook/test.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>  
+#include <fcntl.h>  
+int main()  
+{  
+    while(1)  
+    {  
+        getchar();  
+        printf("hello");  
+        fflush(stdout);  
+    }  
+    return 0;  
+}  
